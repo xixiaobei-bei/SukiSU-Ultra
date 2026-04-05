b5c19033f4ea6d5b87ceacbc449aa6a939cbef71
xixiaobei <110380709+xixiaobei-bei@users.noreply.github.com>
2026-04-05 07:49:15 +0000

    kernel: ported and fixed ksu.c from ReSukiSU.

--- kernel/ksu.c
+++ kernel/ksu.c
@@ -1,3 +1,18 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <linux/export.h>
+#include <linux/fs.h>
+#include <linux/kobject.h>
+#include <linux/module.h>
+#include <linux/rcupdate.h>
+#include <generated/utsrelease.h>
+#include <generated/compile.h>
+#include <linux/version.h>
+
+#ifdef CONFIG_KSU_SUSFS
+#include <linux/susfs.h>
+#endif
+#include <linux/sched.h>
+
 #include "kernel_includes.h"
 
 // uapi
@@ -48,57 +63,139 @@
 
 #ifdef CONFIG_KSU_MANUAL_SU
 #include "other/manual_su.h"
-#include "other/manual_su.c"
 #endif
 
 #ifdef CONFIG_KPM
 #include "kpm/kpm.h"
 #include "kpm/compact.h"
 #include "kpm/super_access.h"
-#include "kpm/kpm.c"
-#include "kpm/compact.c"
-#include "kpm/super_access.c"
 #endif
 
-// unity build
-#include "policy/allowlist.c"
-#include "policy/app_profile.c"
-#include "policy/feature.c"
-#include "manager/apk_sign.c"
-#include "manager/throne_tracker.c"
-#include "manager/pkg_observer.c"
-
-#include "supercall/perm.c"
-#include "supercall/dispatch.c"
-#include "supercall/supercall.c"
-
-#include "infra/su_mount_ns.c"
-#include "infra/file_wrapper.c"
-#include "infra/event_queue.c"
-
-#include "feature/kernel_umount.c"
-#include "feature/sucompat.c"
-#include "feature/sulog.c"
-#include "runtime/ksud.c"
-
-#include "sulog/event.c"
-#include "sulog/fd.c"
-
-#include "hook/lsm_hook.c"
-
-#include "selinux/selinux.c"
-#include "selinux/sepolicy.c"
-#include "selinux/rules.c"
-
-#include "infra/kernel_compat.c"
+#ifdef KSU_TP_HOOK
+#include "syscall_hook_manager.h"
+#include "hook/syscall_hook.h"
+#elif defined(CONFIG_KSU_MANUAL_HOOK)
+#include "hook/lsm_hook.h"
+#endif
 
 struct cred *ksu_cred;
 
 bool allow_shell = IS_ENABLED(CONFIG_KSU_DEBUG);
 module_param(allow_shell, bool, 0);
 
+bool ksu_late_loaded;
+static bool ksu_boot_completed;
+
+extern void ksu_feature_init(void);
+extern void ksu_supercalls_init(void);
+extern void ksu_supercalls_exit(void);
+extern void ksu_allowlist_init(void);
+extern void ksu_allowlist_exit(void);
+extern void ksu_load_allow_list(void);
+extern void ksu_throne_tracker_init(void);
+extern void ksu_throne_tracker_exit(void);
+extern void ksu_observer_init(void);
+extern void ksu_observer_exit(void);
+extern void ksu_file_wrapper_init(void);
+extern void ksu_file_wrapper_exit(void);
+extern void ksu_ksud_init(void);
+extern void ksu_ksud_exit(void);
+extern void ksu_sulog_init(void);
+extern void ksu_sulog_exit(void);
+extern void ksu_kernel_umount_init(void);
+extern void ksu_kernel_umount_exit(void);
+extern void ksu_sucompat_init(void);
+extern void ksu_sucompat_exit(void);
+extern void ksu_setuid_hook_init(void);
+extern void ksu_setuid_hook_exit(void);
+extern void apply_kernelsu_rules(void);
+extern void cache_sid(void);
+extern void setup_ksu_cred(void);
+extern void escape_to_root_for_init(void);
+extern void track_throne(bool, bool);
+extern bool getenforce(void);
+extern void setenforce(bool);
+extern void ksu_dynamic_manager_init(void);
+extern void ksu_dynamic_manager_exit(void);
+
+static inline void ksu_hook_init(void)
+{
+#if defined(KSU_TP_HOOK)
+    ksu_syscall_hook_init();
+    ksu_syscall_hook_manager_init();
+#elif defined(CONFIG_KSU_MANUAL_HOOK)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 8, 0)
+    ksu_lsm_hook_init();
+#endif
+#elif defined(CONFIG_KSU_SUSFS)
+    susfs_init();
+#else
+#error "Unsupported hook type"
+#endif
+}
+
+static inline void ksu_hook_exit(void)
+{
+#if defined(KSU_TP_HOOK)
+    ksu_syscall_hook_manager_exit();
+#else
+    ksu_sucompat_exit();
+    ksu_setuid_hook_exit();
+#endif
+}
+
+#if defined(CONFIG_STACKPROTECTOR) &&                                                                                  \
+    (defined(CONFIG_ARM64) && defined(MODULE) && !defined(CONFIG_STACKPROTECTOR_PER_TASK))
+#include <linux/stackprotector.h>
+#include <linux/random.h>
+unsigned long __stack_chk_guard __ro_after_init __attribute__((visibility("hidden")));
+
+__attribute__((no_stack_protector)) void ksu_setup_stack_chk_guard()
+{
+    unsigned long canary;
+
+    get_random_bytes(&canary, sizeof(canary));
+    canary ^= LINUX_VERSION_CODE;
+    canary &= CANARY_MASK;
+    __stack_chk_guard = canary;
+}
+
+__attribute__((naked)) int __init kernelsu_init_early(void)
+{
+    asm("mov x19, x30;\n"
+        "bl ksu_setup_stack_chk_guard;\n"
+        "mov x30, x19;\n"
+        "b kernelsu_init;\n");
+}
+#define NEED_OWN_STACKPROTECTOR 1
+#else
+#define NEED_OWN_STACKPROTECTOR 0
+#endif
+
 int __init kernelsu_init(void)
 {
+    pr_info("Initialized on: %s (%s) with driver version: %u\n", UTS_RELEASE, UTS_MACHINE, KSU_VERSION);
+#ifdef MODULE
+    ksu_late_loaded = (current->pid != 1);
+#else
+    ksu_late_loaded = false;
+#endif
+
+#if defined(KSU_TP_HOOK) && defined(__x86_64__)
+    if (!boot_cpu_has(X86_FEATURE_INDIRECT_SAFE)) {
+        pr_alert("*************************************************************");
+        pr_alert("**     NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE    **");
+        pr_alert("**                                                         **");
+        pr_alert("**        X86_FEATURE_INDIRECT_SAFE is not enabled!        **");
+        pr_alert("**      KernelSU will abort initialization to prevent      **");
+        pr_alert("**                     kernel panic.                       **");
+        pr_alert("**                                                         **");
+        pr_alert("**     NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE    **");
+        pr_alert("*************************************************************");
+        return -ENOSYS;
+    }
+#endif
+
 #ifdef CONFIG_KSU_DEBUG
     pr_alert("*************************************************************");
     pr_alert("**     NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE    **");
@@ -122,32 +219,95 @@ int __init kernelsu_init(void)
 
     ksu_supercalls_init();
 
-    ksu_lsm_hook_init();
-
+    ksu_setuid_hook_init();
     ksu_sucompat_init();
 
-    ksu_sulog_init();
+    if (ksu_late_loaded) {
+        pr_info("late load mode, skipping kprobe hooks\n");
 
-    ksu_kernel_umount_init();
+        apply_kernelsu_rules();
+        cache_sid();
+        setup_ksu_cred();
+        escape_to_root_for_init();
 
-    ksu_allowlist_init();
+        ksu_allowlist_init();
+        ksu_load_allow_list();
 
-    ksu_throne_tracker_init();
+        ksu_hook_init();
 
-    ksu_ksud_init();
+        ksu_throne_tracker_init();
+        ksu_observer_init();
+        ksu_file_wrapper_init();
 
-    ksu_file_wrapper_init();
+        ksu_sulog_init();
+        ksu_dynamic_manager_init();
 
-#ifdef CONFIG_KSU_SUSFS
-    susfs_init();
+        ksu_boot_completed = true;
+        track_throne(false, true);
+
+        if (!getenforce()) {
+            pr_info("Permissive SELinux, enforcing\n");
+            setenforce(true);
+        }
+    } else {
+        ksu_hook_init();
+
+        ksu_allowlist_init();
+
+        ksu_throne_tracker_init();
+
+        ksu_ksud_init();
+
+        ksu_file_wrapper_init();
+    }
+
+#ifdef MODULE
+#ifndef CONFIG_KSU_DEBUG
+    kobject_del(&THIS_MODULE->mkobj.kobj);
 #endif
-
+#endif
     return 0;
 }
-device_initcall(kernelsu_init);
 
-/*
+void __exit kernelsu_exit(void)
+{
+    ksu_hook_exit();
+    ksu_supercalls_exit();
+    if (!ksu_late_loaded)
+        ksu_ksud_exit();
+    ksu_dynamic_manager_exit();
+    ksu_sulog_exit();
+
+    synchronize_rcu();
+    ksu_observer_exit();
+    ksu_throne_tracker_exit();
+    ksu_allowlist_exit();
+    ksu_file_wrapper_exit();
+    ksu_feature_exit();
+    ksu_kernel_umount_exit();
+    ksu_sucompat_exit();
+    ksu_setuid_hook_exit();
+
+    if (ksu_cred) {
+        put_cred(ksu_cred);
+    }
+}
+
+#if NEED_OWN_STACKPROTECTOR
+module_init(kernelsu_init_early);
+#else
+module_init(kernelsu_init);
+#endif
+module_exit(kernelsu_exit);
+
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("weishu");
 MODULE_DESCRIPTION("Android KernelSU");
-*/
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 0, 0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 13, 0)
+MODULE_IMPORT_NS("VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver");
+#else
+MODULE_IMPORT_NS(VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver);
+#endif
+#endif
